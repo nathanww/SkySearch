2d2af5227013b3ec7824c27104dafe296fa40998
nathanww <nathanww@gmail.com>
2024-12-01 02:45:22 -0500

    Loop processing
    Former-commit-id: 4ca4bbbe15f269f17c2d3f228749266f706dcb11

--- process_saved_images.py
+++ process_saved_images.py
@@ -47,34 +47,42 @@ def examineFiles(im1,im2):
     return result
 
 print("Startup")
-# Get a list of all files in the folder
-files = glob.glob(os.path.join(os.getcwd(), '*.png'))
-print(str(files))
-# Sort files by creation date
-files.sort(key=lambda x: os.path.getctime(x))
+while True:
+    try:
+        # Get a list of all files in the folder
+        files = glob.glob(os.path.join(os.getcwd(), '*.png'))
+        print(str(files))
+        # Sort files by creation date
+        files.sort(key=lambda x: os.path.getctime(x))
 
-# Iterate through the sorted files
-for filen in range(0,len(files)):
-   file=files[filen]
-   textFileExists=os.path.isfile(str(file).replace(".png",".txt")) #if this file already has a text file with findings, then ignore it. But if it doesn't, process it.
-   if not textFileExists:
-       print("Reading "+str(file))
-       im1=image_to_b64(file)
-       webcamid=os.path.basename(file).split("_")[0] #todo: use a different character to seprate webcam id
-       for filem in range(filen+1,len(files)):
-           newfile=files[filem]
-           if webcamid in os.path.basename(newfile): #we have a second image for this webcam
-               im2=image_to_b64(newfile)
-               print("Scanning "+  os.path.basename(file)+","+ os.path.basename(newfile))
-               result=examineFiles(im1,im2)
-               if "NONE" not in result: #if llaava thinks there is something interesting, save that description and the images!
-                   outFile=open(os.path.basename(file).replace(".png",".txt"),'w')
-                   outFile.write(result)
-                   outFile.flush()
-                   outFile.close()
-               else: #otherwise, remove the file to save space
-                    os.remove(file)
-               break
+        # Iterate through the sorted files
+        for filen in range(0,len(files)):
+           file=files[filen]
+           textFileExists=os.path.isfile(str(file).replace(".png",".txt")) #if this file already has a text file with findings, then ignore it. But if it doesn't, process it.
+           if not textFileExists:
+               print("Reading "+str(file))
+               im1=image_to_b64(file)
+               webcamid=os.path.basename(file).split("_")[0] #todo: use a different character to seprate webcam id
+               for filem in range(filen+1,len(files)):
+                   newfile=files[filem]
+                   if webcamid in os.path.basename(newfile): #we have a second image for this webcam
+                       im2=image_to_b64(newfile)
+                       print("Scanning "+  os.path.basename(file)+","+ os.path.basename(newfile))
+                       result=examineFiles(im1,im2)
+                       if "NONE" not in result: #if llaava thinks there is something interesting, save that description and the images!
+                           outFile=open(os.path.basename(file).replace(".png",".txt"),'w')
+                           outFile.write(result)
+                           outFile.flush()
+                           outFile.close()
+                       else: #otherwise, remove the file to save space
+                            os.remove(file)
+                       break
+        print("Finished, checking for new files!")
+        time.sleep(5)
+    except Exception as e:
+        print("Error occurred "+e)
+        time.sleep(5)
+        
            
            
        
