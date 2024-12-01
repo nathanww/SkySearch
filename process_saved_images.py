cb81eaf40c026ce2bc85af5f9854e55c34796548
nathanww <nathanww@gmail.com>
2024-12-01 02:41:58 -0500

    More auto image processing
    Scan saved images, remove uninteresting ones and add create text files with the descriptions of interesting ones.
    
    
    Former-commit-id: ec6d714b73265e08eed63707eeb06e38fca1830d

--- process_saved_images.py
+++ process_saved_images.py
@@ -44,7 +44,7 @@ def examineFiles(im1,im2):
 
     print("Waiting for llava result")
     result = generate_text(prompt,im1,im2)
-    print(result)
+    return result
 
 print("Startup")
 # Get a list of all files in the folder
@@ -56,16 +56,25 @@ files.sort(key=lambda x: os.path.getctime(x))
 # Iterate through the sorted files
 for filen in range(0,len(files)):
    file=files[filen]
-   print("Reading "+str(file))
-   im1=image_to_b64(file)
-   webcamid=os.path.basename(file).split("_")[0] #todo: use a different character to seprate webcam id
-   for filem in range(filen+1,len(files)):
-       newfile=files[filem]
-       if webcamid in os.path.basename(newfile): #we have a second image for this webcam
-           im2=image_to_b64(newfile)
-           print("Scanning "+  os.path.basename(file)+","+ os.path.basename(newfile))
-           examineFiles(im1,im2)
-           break
+   textFileExists=os.path.isfile(str(file).replace(".png",".txt")) #if this file already has a text file with findings, then ignore it. But if it doesn't, process it.
+   if not textFileExists:
+       print("Reading "+str(file))
+       im1=image_to_b64(file)
+       webcamid=os.path.basename(file).split("_")[0] #todo: use a different character to seprate webcam id
+       for filem in range(filen+1,len(files)):
+           newfile=files[filem]
+           if webcamid in os.path.basename(newfile): #we have a second image for this webcam
+               im2=image_to_b64(newfile)
+               print("Scanning "+  os.path.basename(file)+","+ os.path.basename(newfile))
+               result=examineFiles(im1,im2)
+               if "NONE" not in result: #if llaava thinks there is something interesting, save that description and the images!
+                   outFile=open(os.path.basename(file).replace(".png",".txt"),'w')
+                   outFile.write(result)
+                   outFile.flush()
+                   outFile.close()
+               else: #otherwise, remove the file to save space
+                    os.remove(file)
+               break
            
            
        
