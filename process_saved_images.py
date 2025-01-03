5cbf0a595325fc209c620c833d0b27a2e29ceec0
nathanww <nathanww@gmail.com>
2025-01-03 15:51:34 -0500

    fix an issue where two webcams could be confused if their names began with the same sequence

--- process_saved_images.py
+++ process_saved_images.py
@@ -79,32 +79,35 @@ while True:
            textFileExists=os.path.isfile(str(file).replace(".png",".txt")) #if this file already has a text file with findings, then ignore it. But if it doesn't, process it.
            if not textFileExists:
                print("Reading "+str(file))
-               im1=image_to_b64(file)
-               webcamid=os.path.basename(file).split("_")[0] #todo: use a different character to seprate webcam id
-               for filem in range(filen+1,len(files)):
-                   newfile=files[filem]
-                   if webcamid in os.path.basename(newfile): #we have a second image for this webcam
-                       im2=image_to_b64(newfile)
-                       print("Scanning "+  os.path.basename(file)+","+ os.path.basename(newfile))
-                       result=examineFiles(im1,im2)
-                       isUnusual=classify_desc("Read the description below and say DETECT if it describes at least slightly unusual aircraft or at least slightly unusual unusual phenomena present in the sky . Say NODETECT and nothing else if there are no unusual aircraft or unusual phenomena present in the sky. :\n"+result)                       
-                       print(isUnusual)
-                       print(result)
-                        
-                       if "nodetect" not in isUnusual.lower(): #if llaava thinks there is something interesting, save that description and the images!
-                           outFile=open(os.path.basename(file).replace(".png",".txt"),'w')
-                           outFile.write(result)
-                           outFile.flush()
-                           outFile.close()
-                           preserveFile=webcamid
-                           
-                           
-                       else: 
-                            if webcamid in preserveFile: #if this is the last image after an interesting one from the same webcam, preserve it
-                                preserveFile=""
-                            else:
-                                os.remove(file)
-                       break
+               try:
+                   im1=image_to_b64(file)
+                   webcamid=os.path.basename(file).split("_")[0] #todo: use a different character to seprate webcam id
+                   for filem in range(filen+1,len(files)):
+                       newfile=files[filem]
+                       if webcamid==os.path.basename(newfile).split("_")[0]: #we have a second image for this webcam
+                           im2=image_to_b64(newfile)
+                           print("Scanning "+  os.path.basename(file)+","+ os.path.basename(newfile))
+                           result=examineFiles(im1,im2)
+                           isUnusual=classify_desc("Read the description below and say DETECT if it describes at least slightly unusual aircraft or at least slightly unusual unusual phenomena present in the sky . Say NODETECT and nothing else if there are no unusual aircraft or unusual phenomena present in the sky. :\n"+result)                       
+                           print(isUnusual)
+                           print(result)
+                            
+                           if "nodetect" not in isUnusual.lower(): #if llaava thinks there is something interesting, save that description and the images!
+                               outFile=open(os.path.basename(file).replace(".png",".txt"),'w')
+                               outFile.write(result)
+                               outFile.flush()
+                               outFile.close()
+                               preserveFile=webcamid
+                               
+                               
+                           else: 
+                                if webcamid in preserveFile: #if this is the last image after an interesting one from the same webcam, preserve it
+                                    preserveFile=""
+                                else:
+                                    os.remove(file)
+                           break
+               except OSError: #often occurs if a file is corrupted
+                    pass
         print("Finished, checking for new files!")
         time.sleep(1)
     except Exception as e:
