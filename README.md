21f9e523a233e5ab4f0399b9d7e2b59752912397
nathanww <nathanww@gmail.com>
2025-01-03 16:13:02 -0500

    Update info on prompts and testing with new prompt

--- README.md
+++ README.md
@@ -30,7 +30,19 @@
  
  By default, webcamdl will save a new frame every 30 seconds. process_saved_images will iterate through all images in the folder, and classify them as interesting or uninteresting. Uninsteresting images are deleted while interesting imagaes containing a possible UAP are saved. FOr each interesting image, a text file is also saved containing the model's description of the image. Image acqusition can run at the same time as processing the saved images to create a continuous pipeline.
  
+ #LLaVA prompts
+ Currently we use the following prompt to describe photos:
  
+describe any unusual aircraft or unusual phenomena present in the sky in these photos. CRITICAL: do not describe anything that is not in the sky. It is CRITICAL you accurately describe any possibly unusual phenomena in the sky (only). The photo on the right is 60 seconds after the photo on the left so you can tell how things are moving. If an object is moving unusually, make sure to note this. Describe ONLY things in the sky and nothing else. Do not speculate about the things you observe. Ignore any text or watermarks on the image.
+ 
+ And a second prompt to evaluate whether the description given by the frist prompt contains anything "unusual"
+ 
+Read the description below and say DETECT if it describes at least slightly unusual aircraft or at least slightly unusual unusual phenomena present in the sky . Say NODETECT and nothing else if there are no unusual aircraft or unusual phenomena present in the sky.                       
+
+ 
+
+ 
+
  # Evaluation
  We tested the system against a positive and negative test set. The postiive test set contains 4 videos with phenomena human-identified as cotnaining UAPs or unusual phenomena, consisting of (1) Starlink string at night (2) Video of a small unidentified UAP against clouds during the day, , (3) video of a group of UAPs seen from a suburb at night, and (4) Video of the Phoenix lights from a rural area
  The negative set contains videos from 4 webcam feeds: Chicago skyline (day), Las vegas skyline (day), Malaga airport (night), and the Pendaries area of New Mexico (day)
@@ -39,3 +51,6 @@
  
  
  
+# Experiment 2--prompts for faster processing
+
+We asked the system to give more concise descriptions of the scenes in order to reduce image processing time by reducing the number of tokens produced for the description--we added the requirement to the end of the description prompt =--"Use less than 6 words if you don't see anything unusual.". However, this dramatically reduced sensitivity (only 1 frame of 1 video in the positive test was correctly identified).
---
+++ positivetest - newprompt/process_saved_images_test.py
@@ -0,0 +1,127 @@
+import requests
+import json
+from PIL import Image
+import base64
+from io import BytesIO
+import glob
+import os
+import time
+def image_to_b64(imagePath):
+    image = Image.open(imagePath)
+
+    # Convert the image to bytes
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    image_bytes = buffered.getvalue()
+
+    # Encode the image to base64
+    image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+    return image_base64
+
+def generate_text(prompt,im1,im2,model="llava"):
+    url = "http://localhost:11434/api/generate"
+    
+    data = {
+        "model": model,
+        "prompt": prompt,
+        "stream": False,
+        "images":[im1,im2]
+    }
+
+    response = requests.post(url, json=data)
+    
+    if response.status_code == 200:
+        result = response.json()
+        return result["response"]
+    else:
+        return f"Error: {response.status_code}, {response.text}"
+
+
+def classify_desc(prompt,model="llava"):
+    url = "http://localhost:11434/api/generate"
+    
+    data = {
+        "model": model,
+        "prompt": prompt,
+        "stream": False,
+    }
+
+    response = requests.post(url, json=data)
+    
+    if response.status_code == 200:
+        result = response.json()
+        return result["response"]
+    else:
+        return f"Error: {response.status_code}, {response.text}"
+        
+def examineFiles(im1,im2):
+
+    prompt = ''''describe any unusual aircraft or unusual phenomena present in the sky in these photos. CRITICAL: do not describe anything that is not in the sky. It is CRITICAL you accurately describe any possibly unusual phenomena in the sky (only). The photo on the right is 60 seconds after the photo on the left so you can tell how things are moving. If an object is moving unusually, make sure to note this. Describe ONLY things in the sky and nothing else. Do not speculate about the things you observe. Ignore any text or watermarks on the image. Use less than 6 words if you don't see anything unusual.'''
+    #prompt="describe these images"
+
+    print("Waiting for llava result")
+    result = generate_text(prompt,im1,im2)
+    return result
+
+print("Startup")
+while True:
+    try:
+        # Get a list of all files in the folder
+        files = glob.glob(os.path.join(os.getcwd(), '*.png'))
+        print(str(files))
+        # Sort files by creation date
+        files.sort(key=lambda x: os.path.getctime(x))
+        preserveFile=""
+        # Iterate through the sorted files
+        for filen in range(0,len(files)):
+           file=files[filen]
+          
+           textFileExists=os.path.isfile(str(file).replace(".png",".txt")) #if this file already has a text file with findings, then ignore it. But if it doesn't, process it.
+           if not textFileExists:
+               print("Reading "+str(file))
+               try:
+                   im1=image_to_b64(file)
+                   webcamid=os.path.basename(file).split("_")[0] #todo: use a different character to seprate webcam id
+                   for filem in range(filen+1,len(files)):
+                       newfile=files[filem]
+                       if webcamid==os.path.basename(newfile).split("_")[0]: #we have a second image for this webcam
+                           im2=image_to_b64(newfile)
+                           print("Scanning "+  os.path.basename(file)+","+ os.path.basename(newfile))
+                           result=examineFiles(im1,im2)
+                           isUnusual=classify_desc("Read the description below and say DETECT if it describes at least slightly unusual aircraft or at least slightly unusual unusual phenomena present in the sky . Say NODETECT and nothing else if there are no unusual aircraft or unusual phenomena present in the sky. :\n"+result)                       
+                           print(isUnusual)
+                           print(result)
+                            
+                           if "nodetect" not in isUnusual.lower(): #if llaava thinks there is something interesting, save that description and the images!
+                               outFile=open(os.path.basename(file).replace(".png",".txt"),'w')
+                               outFile.write(result)
+                               outFile.flush()
+                               outFile.close()
+                               preserveFile=webcamid
+                               
+                               
+                           else: 
+                                if webcamid in preserveFile: #if this is the last image after an interesting one from the same webcam, preserve it
+                                    preserveFile=""
+                                else:
+                                    pass
+                           break
+               except OSError: #often occurs if a file is corrupted
+                    pass
+        print("Finished, checking for new files!")
+        time.sleep(1)
+    except Exception as e:
+        print("Error occurred "+str(e))
+        time.sleep(5)
+        
+           
+           
+       
+       
+
+    
+#loop through all 
+#im1=image_to_b64("output-1.png")
+#im2=image_to_b64("output-2.png")
+
+
